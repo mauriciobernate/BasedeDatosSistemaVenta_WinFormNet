23ddc000a2236216eda6b25f4f56af48723f69f4
JOHAN MAURICIO BERNATE AYA <johanmauriciobernateaya@gmail.com>
2026-03-06 11:51:45 -0500

    Add files via upload
    Removed the NombreLogo and UrlLogo fields from the insert and update procedures for the Negocio table.

--- Recursos parte 03/script_negocio.sql
+++ Recursos parte 03/script_negocio.sql
@@ -5,14 +5,14 @@
  sp_help negocio
  select * from Negocio
 
- insert into Negocio(RazonSocial,RUC,Direccion,Celular,Correo,SimboloMoneda,NombreLogo,UrlLogo)
- values ('CodigoEstudiante','100100100','av.esperanza 123','900800700','ce@gmail.com','S/.','','')
+ insert into Negocio(RazonSocial,RUC,Direccion,Celular,Correo,SimboloMoneda)
+ values ('CodigoEstudiante','100100100','av.esperanza 123','900800700','ce@gmail.com','$.')
 
 
  CREATE PROCEDURE sp_obtenerNegocio
  as
  begin
-	 select RazonSocial,RUC,Direccion,Celular,Correo,SimboloMoneda,NombreLogo,UrlLogo
+	 select RazonSocial,RUC,Direccion,Celular,Correo,SimboloMoneda
 	 from Negocio where IdNegocio = 1
  end
 
@@ -26,9 +26,7 @@
 @Direccion varchar(100),
 @Celular varchar(10),
 @Correo varchar(30),
-@SimboloMoneda varchar(5),
-@NombreLogo varchar(100),
-@UrlLogo varchar(200)
+@SimboloMoneda varchar(5)
  )
  as
  begin
@@ -38,8 +36,7 @@
 	Direccion = @Direccion,
 	Celular = @Celular,
 	Correo = @Correo,
-	SimboloMoneda = @SimboloMoneda,
-	NombreLogo = @NombreLogo,
-	UrlLogo = @UrlLogo
+	SimboloMoneda = @SimboloMoneda
 	where IdNegocio = 1
+
  end
